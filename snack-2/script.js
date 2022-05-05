b9b57ad5a299970a682ab6a0661f19fd0b4d35ae
Scarpaz14 <scarparofrancesco14@gmail.com>
2022-05-05 17:11:26 +0200

    unito i due for

--- snack-2/script.js
+++ snack-2/script.js
@@ -7,11 +7,11 @@ let arrayList= [];
 
 for( let i = 0; i < 10; i++ ) {
     arrayList.push(Math.floor(Math.random() * 100) + 1);
+    for( let arrayTwo= 0; arrayTwo < n; arrayTwo++){
+        console.log(arrayList);
+    }
 }
 
-for( let arrayTwo= 0; arrayTwo < n; arrayTwo++){
-    console.log(arrayList);
-}
 
 
 
