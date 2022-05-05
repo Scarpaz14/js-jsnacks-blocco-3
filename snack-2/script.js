5b3e1c21cf5ebcda64fa479d653e1327f2e45802
Scarpaz14 <scarparofrancesco14@gmail.com>
2022-05-05 16:46:45 +0200

    aggiunto codice js

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,13 +1,19 @@
 // l utente inserisce un numero
-const N = Number(prompt('Inserisci un numero'))
+const n = Number(prompt('Inserisci un numero'))
 
 // generiamo tot array in base al numero inserito dall'utente
 
 let arrayList= [];
 
-
 for( let i = 0; i < 10; i++ ) {
     arrayList.push(Math.floor(Math.random() * 100) + 1);
 }
 
-console.log(arrayList);
+for( let arrayTwo= 0; arrayTwo < n; arrayTwo++){
+    console.log(arrayList);
+}
+
+
+
+
+
